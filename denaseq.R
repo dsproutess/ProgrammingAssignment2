7ee7ef5747fce0936ffc0901662dd1fe199149f6
dsproutess <89844658+dsproutess@users.noreply.github.com>
2021-09-01 11:36:58 +0300

    Add files via upload

---
+++ denaseq.R
@@ -0,0 +1,17 @@
+#COMPLEMANTARY DNA "TGCGGCAGCGGCCG"
+
+DNAseq<-function(x){
+  basepair=c("A","T","G","C")
+  abc<-unlist(strsplit(toupper(x), NULL))
+  paste(unlist(lapply(abc, function(DNAseq){
+    if (DNAseq=="A") stringss<-"T"
+    if (DNAseq=="T") stringss<-"A"
+    if (DNAseq=="G") stringss<-"C"
+    if (DNAseq=="C") stringss<-"G"
+    if (!DNAseq%in%basepair) stringss<-"N"
+    return(stringss)
+  })), collapse="")
+}
+DNAseq("TGCGGCAGCGGCCG")
+DNAseq("ATGCCTAOTCGGCAACWGCCA")
+
